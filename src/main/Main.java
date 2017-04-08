5b0af0b9c8faf54108b9370b1d54efa2900e8130
bouixl <shihico@gmail.com>
2017-04-09 01:22:22 +0200

    chargement des commandes

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,18 @@
 package main;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.security.auth.login.LoginException;
 
-import commandes.CmdClear;
-import commandes.CmdHelp;
-import commandes.CmdLeave;
-import commandes.CmdMotto;
-import commandes.CmdPing;
-import commandes.CmdVoice;
+import com.google.common.reflect.ClassPath;
+
+import commandes.Commande;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
@@ -35,12 +34,15 @@ public class Main implements EventListener, ConnectionListener
 	private JDA jda;
 
 	private List<Message> msgTab;
+	
+	private List<Commande> commandesValides;
 
 	public static final String DEFAULT_SEARCH = "nico_robin";
 
 	Main(String token)
 	{	
 		this.msgTab = new ArrayList<Message>();
+		this.commandesValides = new ArrayList<Commande>();
 		
 		try {
 			this.jda = new JDABuilder(AccountType.BOT).setToken(token).setBulkDeleteSplittingEnabled(false).buildBlocking();
@@ -49,7 +51,6 @@ public class Main implements EventListener, ConnectionListener
 			e.printStackTrace();
 		}
 		
-		this.jda.addEventListener(this);
 		System.out.println("Connecté avec: " + this.jda.getSelfUser().getName());
 		int nbServeurs = this.jda.getGuilds().size();
 		System.out.println("Le bot est autorisé sur " + nbServeurs + " serveur" + (nbServeurs > 1 ? "s" : ""));
@@ -59,8 +60,43 @@ public class Main implements EventListener, ConnectionListener
 	public static void main(String[] args)
 	{
 		Main m = new Main("MjgyNTM5NTAyODE4NDI2ODky.C4n-8g.3NGwGhWK8xeugEk2swSe57CxUPo");
+		m.registerCommands();
+		m.jda.addEventListener(m);
 		m.run();
 	}
+	
+    private void registerCommands() {
+    	final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    	
+    	try {
+			for (final ClassPath.ClassInfo info : ClassPath.from(loader).getTopLevelClasses("commandes")) {
+				
+				Class<?> clazz = info.load();
+				Class<?> interfaces[] = clazz.getInterfaces();
+				for(Class<?> c:interfaces) {
+					if(c.equals(Commande.class))
+					{
+						try {
+							Commande nouvelleCommande = (Commande) clazz.newInstance();
+							Optional<Commande> commandeExistante = this.commandesValides.stream().filter(cmd -> cmd.getName().equalsIgnoreCase(nouvelleCommande.getName())).findAny();
+			                if (!commandeExistante.isPresent()) {
+			                    this.commandesValides.add(nouvelleCommande);
+			                    System.out.println("Commande enregistrée: "+ nouvelleCommande.getName());
+			                } else {
+			                	System.out.println("Enregistrement de deux commandes portant le même nom: " + commandeExistante.get().getName());
+			                	System.out.println("Existante: " + commandeExistante.get().getClass().getName());
+			                	System.out.println("Nouvelle: " + nouvelleCommande.getClass().getName());
+			                }
+						} catch (InstantiationException | IllegalAccessException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
 
 	private void run() {
 		boolean stop = false;
@@ -73,6 +109,12 @@ public class Main implements EventListener, ConnectionListener
 				System.out.println("Arrêt demandé");
 				this.jda.shutdown(true);
 				stop = true;
+			}
+			else if (cmd.equalsIgnoreCase("regCmd"))
+			{
+				System.out.println("Recherche de commandes");
+				this.registerCommands();
+				System.out.println("Recherche terminée");
 			}
 		}
 		scanner.close();
@@ -105,34 +147,18 @@ public class Main implements EventListener, ConnectionListener
 		}
 	}
 
-	private void lireCommande(MessageReceivedEvent e, String commande, String arguments) {
+	private void lireCommande(MessageReceivedEvent e, String cmdString, String arguments) {
 		boolean result = false;
-		switch(commande)
-		{
-			case "motto":
-				result = new CmdMotto().run(this, e, arguments);
-				break;
-			case "mottohelp":
-				result = new CmdHelp().run(this, e, arguments);
-				break;
-			case "mottovoice":
-				result = new CmdVoice().run(this, e, arguments);
-				break;
-			case "mottoleave":
-				result = new CmdLeave().run(this, e, arguments);
-				break;
-			case "mottoclear":
-				result = new CmdClear().run(this, e, arguments);
-				break;
-			case "ping":
-				result = new CmdPing().run(this, e, arguments);
-				break;
-			default:
-				// Commande non reconnue
-				break;
-		}
-		if(!result)
-			System.out.println("Commande non reconnue ou problème d'execution.");
+		
+        Optional<Commande> commande = this.commandesValides.stream()
+                .filter(com -> com.getName().equalsIgnoreCase(cmdString) || (com.getAliases() != null && com.getAliases().contains(cmdString)))
+                .findAny();
+        if (commande.isPresent()) {
+        	// La commande existe
+            result = commande.get().run(this, e, arguments);
+        } else {
+        	// Commande inconnue
+        }
 	}
 
 	@Override
