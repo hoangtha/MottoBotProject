2dd355db776821ea779003a0492f8d6a872d9a1f
bouixl <shihico@gmail.com>
2017-04-09 14:15:03 +0200

    help mieux

--- src/commandes/CmdHelp.java
+++ src/commandes/CmdHelp.java
@@ -3,6 +3,8 @@ package commandes;
 import java.util.List;
 
 import main.MottoBot;
+import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class CmdHelp implements Commande {
@@ -21,19 +23,21 @@ public class CmdHelp implements Commande {
 	public void run(MottoBot bot, MessageReceivedEvent e, String arguments) {
 		bot.addMsg(e.getMessage());
 		e.getChannel().sendTyping();
-		e.getChannel()
-				.sendMessage(
-						"```Liste des commandes :\n"
-						+ "=motto [tag]:     Affiche un lien vers l'image voulue\n"
-						+ "=mottoclear :      Nettoie un peu\n"
-						+ "=mottohelp :       Affiche les commandes disponible\n\n"
-						+ "PLAYER : \n"
-						+ "=mottoplay (url) : Rejoint le channel vocal Taverne et lance la musique si possible\n"
-						+ "=mottoleave :      Quitte le channel vocal et vide la playlist\n"
-						+ "=mottoskip :       Passe à la prochaine musique dans la playlist\n"
-						+ "=mottofplay (url): Force le lancement de la musique\n\n\n"
-						+ "Liste des sources pour le player : YouTube, SoundCloud, Bandcamp, Vimeo, Twitch stream, HTTP URLs, (local)```")
-				.queue();
+		MessageBuilder mb = new MessageBuilder();
+		mb.append("```");
+		mb.append("Liste des commandes :\n");
+		mb.append("=motto [tag]:      Affiche un lien vers l'image voulue\n");
+		mb.append("=mottoclear :      Nettoie un peu\n");
+		mb.append("=mottohelp :       Affiche les commandes disponible\n");
+		mb.append("\nPlayer :\n");
+		mb.append("=mottoplay (url) : Rejoint le channel vocal Taverne et lance la musique si possible\n");
+		mb.append("=mottoleave :      Quitte le channel vocal et vide la playlist\n");
+		mb.append("=mottoskip :       Passe à la prochaine musique dans la playlist\n");
+		mb.append("=mottofplay (url): Force le lancement de la musique\n");
+		mb.append("\nListe des sources pour le player : YouTube, SoundCloud, Bandcamp, Vimeo, Twitch stream, HTTP URLs, (local)");
+		mb.append("```");
+		Message m = mb.build();
+		e.getChannel().sendMessage(m).queue();
 	}
 
 }
