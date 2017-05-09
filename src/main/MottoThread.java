9c5030f02bafc8e06d9827d01d27576d5a23b518
Thai Binh <momojean0@gmail.com>
2017-05-09 18:13:36 +0200

    Revert "apparament, c'est pas safe"
    This reverts commit 0c75a23b1c9943c339e15c74a80b14f57caae411.

--- src/main/MottoThread.java
+++ src/main/MottoThread.java
@@ -61,11 +61,7 @@ public class MottoThread implements Runnable {
 				if(this.e.getAuthor().getId().equals("259789587432341506")) {
 					selector = SANKAKU;
 					nbRecherche = MAX_TRIES-1;
-					searchUrl = "https://chan.sankakucomplex.com/?tags=order:random+nico_robin+solo-rating:explicit";
-					if(!channelIsNSFW)
-					{
-						searchUrl += "-rating:questionnable";
-					}
+					searchUrl = "https://chan.sankakucomplex.com/?tags=order:random+nico_robin+solo+-rating:explicit";
 				}
 				else {
 					this.e.getChannel().sendMessage("no").queue();
