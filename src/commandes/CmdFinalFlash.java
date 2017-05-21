18abc6960fab5806b1f858553fea98364fea7a1a
rougen <nicolasrouge38@gmail.com>
2017-05-21 12:06:23 +0200

    pourquoi c'est là?

--- src/commandes/CmdFinalFlash.java
+++ src/commandes/CmdFinalFlash.java
@@ -12,7 +12,9 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 public class CmdFinalFlash implements Commande {
 	
 	public static final String FINALFLASH = "https://puu.sh/vWsxH/0c39eaf499.gif" ;
-	public static final Color FLASH = new Color(227, 140, 45);
+	public static final String FINALFAIL = "https://puu.sh/vWQaM/b3a72c682c.gif";
+	public static final String FINALGIN = "https://puu.sh/vWQoz/698f0fc128.gif";
+	public static final Color FLASH = new Color(255, 250, 10);
 
 	@Override
 	public String getName() {
@@ -32,20 +34,28 @@ public class CmdFinalFlash implements Commande {
 		//ID Momojean <@262610896545644554>
 		//ID Ademage <@232603027641466882>
 		Member Momojean = e.getGuild().getMemberById("262610896545644554");
+		EmbedBuilder gif = new EmbedBuilder();
+		gif.setColor(FLASH);
 		
 		
 		if(e.getAuthor().getId().equals("232603027641466882"))
 		{
-			EmbedBuilder gif = new EmbedBuilder();
-			gif.setColor(FLASH);
+			
 			gif.setImage(FINALFLASH);
-			gif.appendDescription("Pour "+Momojean.getEffectiveName()+" FINAAAL FLAAAAAAAAAAASH!");
+			gif.appendDescription("Pour <@"+Momojean.getUser().getId()+"> FINAAAL FLAAAAAAAAAAASH!");
+			e.getChannel().sendMessage(gif.build()).queue();
+		}		
+		else if (e.getAuthor().getId().equals("259789587432341506"))
+		{
+			gif.setImage(FINALGIN);
+			gif.appendDescription("KA ME HA ME.....HA?");
 			e.getChannel().sendMessage(gif.build()).queue();
 		}
-		
-		else 
+		else
 		{
-			e.getChannel().sendMessage("nope, tu ne sais pas faire ça toi").queue();
+			gif.setImage(FINALFAIL);
+			gif.appendDescription("Oula, tu crois faire koi toi?!");
+			e.getChannel().sendMessage(gif.build()).queue();
 			return;
 		}
 
